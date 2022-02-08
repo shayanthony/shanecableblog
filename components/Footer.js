e27c4ccc770ea24936961687e51c6423432c232d
Shane Cable <cableincorp@gmail.com>
2022-02-08 22:25:40 +0000

    feat(*): Set colors & gradients for light/dark mode

--- components/Footer.js
+++ components/Footer.js
@@ -34,7 +34,7 @@ export default function Footer() {
   function FooterGradient() {
     return (
       <div className="mx-auto max-w-6xl motion-safe:animate-rotate-colors pointer-events-none z-[-1]">
-        <div className="absolute inset-x-0 bg-gradient-to-r from-amber-500 via-indigo-500 to-emerald-500 rounded-t-full opacity-20 blur-3xl h-[200px]" />
+        <div className="absolute inset-x-0 bg-gradient-to-r from-yellow-500 via-indigo-500 to-teal-500 rounded-t-full opacity-20 blur-3xl h-[200px]" />
       </div>
     );
   }
