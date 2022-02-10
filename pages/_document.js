03892a0904695e2bb9cd2c80debd8b46ce480083
Shane Cable <cableincorp@gmail.com>
2022-02-10 22:58:56 +0000

    refactor(*): Update favicons & dark theme (again)

--- pages/_document.js
+++ pages/_document.js
@@ -4,21 +4,15 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
-          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/static/favicons/black-hole.png" />
           <link
             rel="icon"
             type="image/png"
-            sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
+            sizes="60x60"
+            href="/static/favicons/black-hole.png"
           />
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="mask-icon" href="/static/favicons/black-hole.png" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
@@ -29,7 +23,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
         </Head>
-        <body className="bg-violet-50 text-black antialiased dark:bg-slate-900 dark:text-white">
+        <body className="bg-violet-50 text-black antialiased dark:bg-zinc-900 dark:text-white">
           <Main />
           <NextScript />
         </body>
