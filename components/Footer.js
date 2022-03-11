6ccbd69b6a9a693f55f843193ecba40a64828d68
Shane Cable <cableincorp@gmail.com>
2022-03-11 23:17:54 +0000

    fix(*): Make footer mobile friendly

--- components/Footer.js
+++ components/Footer.js
@@ -13,10 +13,10 @@ export default function Footer() {
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
+          <div className="hidden md:block">{siteMetadata.author}</div>
+          <div className="hidden md:block">{` • `}</div>
+          <div className="hidden md:block">{`© ${new Date().getFullYear()}`}</div>
+          <div className="hidden md:block">{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
       </div>
@@ -25,9 +25,9 @@ export default function Footer() {
 
   function FooterGradient() {
     return (
-      <div className="mx-auto max-w-6xl motion-safe:animate-rotate-colors pointer-events-none z-[-1]">
-        <div className="absolute inset-x-0 bg-gradient-to-r from-yellow-500 via-indigo-500 to-teal-500 rounded-t-full opacity-20 blur-3xl h-[200px]" />
+      <div className="motion-safe:animate-rotate-colors pointer-events-none z-[-1] mx-auto max-w-6xl">
+        <div className="absolute inset-x-0 h-[200px] rounded-t-full bg-gradient-to-r from-yellow-500 via-indigo-500 to-teal-500 opacity-20 blur-3xl" />
       </div>
-    );
+    )
   }
 }
