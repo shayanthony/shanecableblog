91986a1ebeb3091a9025d2c667c7ecdc61657d89
Shane Cable <cableincorp@gmail.com>
2022-03-20 06:49:57 +0000

    feat(*): Add first two blog posts

--- data/snippetsData.js
+++ data/snippetsData.js
@@ -1,3 +1,10 @@
-const snippetsData = []
+const snippetsData = [
+  {
+    title: 'MS Dynamics CRM Dirty Form',
+    description: `For when the dreaded unsaved changes rears it's head.`,
+    imgSrc: '/static/images/js.png',
+    href: '/blog/javascript-unsaved-changes',
+  },
+]
 
 export default snippetsData
